866393d2bc6f078eb8b446e73ce63d1f08a950e3
odolha <odolha@gmail.com>
2026-06-28 17:16:39 +0300

    sample/test stages only in non-release mode

--- late-ssh/src/app/arcade/racer/tracks/mod.rs
+++ late-ssh/src/app/arcade/racer/tracks/mod.rs
@@ -3,14 +3,25 @@
 //! Tracks live in their own files in this directory and are registered here.
 
 pub mod presets;
-pub mod test;
-pub mod sample;
 pub mod batin;
+
+#[cfg(debug_assertions)]
+pub mod test;
+#[cfg(debug_assertions)]
+pub mod sample;
 
 use super::track::Track;
 
 /// Every track available in the picker, in display order.
+#[cfg(debug_assertions)]
 pub const ALL_TRACKS: &[&Track] = &[&test::TRACK, &sample::TRACK, &batin::TRACK];
 
+#[cfg(not(debug_assertions))]
+pub const ALL_TRACKS: &[&Track] = &[&batin::TRACK];
+
 /// Default track loaded when none has been selected yet.
+#[cfg(debug_assertions)]
 pub const DEFAULT_TRACK: &Track = &test::TRACK;
+
+#[cfg(not(debug_assertions))]
+pub const DEFAULT_TRACK: &Track = &batin::TRACK;
